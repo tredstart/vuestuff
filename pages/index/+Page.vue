22a617a3537fdc851a556ff87ab8987dee69ebdc
salty <rslnkssk@gmail.com>
2025-05-02 21:02:18 +0200

    pivot to pokedex

--- pages/index/+Page.vue
+++ pages/index/+Page.vue
@@ -9,47 +9,41 @@
         </template>
         <template v-else>
             <li v-for="item in data" :key="item.id">
-                {{ item.name }} ({{ item.url }})
+                {{ item.id }} ({{ item.name }}) ({{ item.weight }})
             </li>
         </template>
     </ol>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 
 import { onServerPrefetch } from 'vue'
 import { useQuery } from '@tanstack/vue-query'
-import { Pokemon } from './types.ts'
 
 const page = 'https://pokeapi.co/api/v2/pokemon?limit=151'
 
 const { isError, isPending, isFetching, data, error, suspense } = useQuery({
-    queryKey: [],
+    queryKey: ['pokemon-list'],
     queryFn: () => fetchPokemonPage(),
     staleTime: 1000 * 60 * 5,
-    select: (data) => minimize(data),
 })
 
 // This will be called on the server to prefetch the data
 onServerPrefetch(suspense)
 
-async function fetchPokemonPage(): Promise<PokemonPage> {
+async function fetchPokemonPage() {
     const response = await fetch(page)
-    const pokemon_page = (await response.json()) as PokemonPage
-    return pokemon_page
+    const pokemon_page = (await response.json())
+    return Promise.all(pokemon_page.results.map(async (poke) => {
+        try {
+            const { name, url } = poke
+            const pokemon_response = await fetch(url)
+            const pokemon = await pokemon_response.json()
+            return { id: pokemon.id, name: pokemon.name, weight: pokemon.weight }
+        } catch (TypeError) {
+            return {}
+        }
+    }))
 }
 
-function minimize(pokemon_page: PokemonPage): Pokemon[] {
-    return pokemon_page.results.map((poke) => {
-        const { name, url } = poke
-    }) as Pokemon[]
-}
-
-function minimizePoke(poke) {
-    return {
-        id: poke.id,
-        name: poke.name,
-        weight: poke.weight,
-    }
-}
 </script>
